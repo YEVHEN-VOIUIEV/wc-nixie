daaf2597dd18cbd9bbc62b7b3edde988b163398b
Yevhen Voiuiev <yevhen.voiuiev@gmail.com>
2020-01-23 19:17:25 +0200

    Add images

--- src/wc-nixie.js
+++ src/wc-nixie.js
@@ -6,24 +6,48 @@ class WebComponentNixie extends HTMLElement {
 
   connectedCallback() {
     this._shadowRoot.innerHTML = `
-          <style>
-              :host {
-                  color: #CCC;
-                  font-size: 32px;
-              }
-          </style>
-          <div class="nixie"></div>`;
+      <style>
+        .clock {
+          display: grid;
+          grid-template-columns: auto auto auto auto auto auto;
+        }
+
+        .digit {
+          width: 100%;
+          border-radius: 50%;
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: 100% 100%;
+          background-color: #070000;
+        }
+
+        .digit:before {
+          content: '';
+          display: block;
+          padding-top: 100%;
+        }
+      </style>
+      <div class="nixie"></div>`;
 
     this.$nixie = this._shadowRoot.querySelector('.nixie');
 
     setInterval(() => {
       const date = new Date();
-      this.$nixie.innerHTML =
+      const timeString =
         ('0' + date.getHours()).slice(-2) +
-        ':' +
         ('0' + date.getMinutes()).slice(-2) +
-        ':' +
         ('0' + date.getSeconds()).slice(-2);
+
+      this.$nixie.innerHTML = `
+        <div class="clock">
+          ${[0, 1, 2, 3, 4, 5]
+            .map(i => {
+              const style = `background-image: url('./images/${timeString[i]}.jpg')`;
+              return `<div class="digit" style="${style}"></div>`;
+            })
+            .join('')}
+        </div>
+        `;
     }, 1000);
   }
 }
